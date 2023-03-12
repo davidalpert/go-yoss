ee1c81a2b3cc8b18675752737af98b2771ac480b
David Alpert <david@spinthemoose.com>
2023-03-12 10:28:25 -0500

    feat: merge one file into another to validate a merge

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -65,6 +65,7 @@ func NewRootCmd(ioStreams printers.IOStreams) *cobra.Command {
 
 	// Register subcommands
 	rootCmd.AddCommand(NewCmdConfig(ioStreams))
+	rootCmd.AddCommand(NewCmdMerge(ioStreams))
 	rootCmd.AddCommand(NewCmdVersion(ioStreams))
 
 	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", fmt.Sprintf("config file (default is $HOME/.%s/config.yaml)", version.Detail.AppName))
